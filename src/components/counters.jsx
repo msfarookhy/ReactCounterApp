87c06d5252677ec9d9663b5cc98196152c8ebd00
msfarookhy <msfarookhy@gmail.com>
2018-11-02 20:56:21 +0530

    Adedd Reset Butoon

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -20,6 +20,9 @@ class Counters extends Component {
     render() {
         return (
             <div>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => (
                     <Counter
                         key={counter.id}
